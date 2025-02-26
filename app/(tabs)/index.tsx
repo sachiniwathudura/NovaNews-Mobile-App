de662bf499faf3e807762de3c9eabf3c7186eebe
sachiniwathudura <sachiniwathudura@gmail.com>
2025-02-27 02:41:38 +0530

    add - index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,87 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+import {StyleSheet, View, Text, ActivityIndicator, ScrollView} from "react-native";
+import Header from "../../components/Header";
+import SearchBar from  "../../components/SearchBar";
+import {useEffect, useState} from "react";
+import {NewsDataType} from "../../types";
+import axios from "axios";
+import BreakingNews from "../../components/BreakingNews";
+import Categories from "../../components/Categories";
+import NewsList from "../../components/NewsList";
+import Loading from "../../components/Loading";
 
 type Props = {}
-
 const Page = (props: Props) => {
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
-  )
-}
+    const {top: safeTop} = useSafeAreaInsets();
+    const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([]);
+    const [news, setNews] = useState<NewsDataType[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-export default Page
+    useEffect(() => {
+        getBreakingNews();
+        getNews();
+    },[]);
+
+    const getBreakingNews = async ()=>{
+        try {
+            const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY }&country=in,lk&language=en,si&category=education,politics,science,technology,world&image=1&removeduplicate=1&size=5`
+            const response = await axios.get(URL);
+
+            // console.log(response.data);
+            if (response && response.data) {
+               setBreakingNews(response.data.results);
+               setIsLoading(false);
+            }
+             // API key = pub_688125271182cc9b928ab4c51d31c9a1b84c2
+            //url 2 ->https://newsdata.io/api/1/news?apikey=pub_688125271182cc9b928ab4c51d31c9a1b84c2&country=in,lk&language=en,si&image=1
+        }catch(err:any){
+          console.log(err.message);
+        }
+    };
+
+    const getNews = async (category:string = '')=>{
+        try {
+            let categoryString = '';
+            if (category.length !== 0) {
+                categoryString = `&category=${category}`
+            }
+            const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY }&country=in,lk,wo&language=en,si&image=1&removeduplicate=1&size=10${categoryString}`;
+            const response = await axios.get(URL);
+
+            console.log(response.data);
+            if (response && response.data) {
+                setNews(response.data.results);
+                setIsLoading(false);
+            }
+        }catch(err:any){
+            console.log(err.message);
+        }
+    };
+
+    const onCatChanged = (category:string)=>{
+        console.log("Category:",category);
+        setNews([]);
+        getNews(category);
+    }
+
+    return (
+        <ScrollView style={[styles.container, {paddingTop: safeTop}]}>
+            <Header/>
+            <SearchBar withHorizontalPadding={true}/>
+            {isLoading ?(
+                <Loading size={'large'}/>
+            ):(
+                <BreakingNews newsList={breakingNews} />
+            )}
+            <Categories onCategoryChanged={onCatChanged}/>
+            <NewsList newsList={news}/>
+        </ScrollView>
+    )
+}
+export default Page;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-})
+    container: {
+        flex: 1,
+    }
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,97 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
-import { useRouter } from "expo-router";
+import {ImageBackground, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {useRouter} from "expo-router";
+import Animated, {FadeInDown, FadeInRight, FlipInEasyX, FlipOutEasyX} from "react-native-reanimated";
+import {StatusBar} from "expo-status-bar";
 
 const Page = () => {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <View style={styles.container}>
-      <Text>Welcome Page</Text>
-      <TouchableOpacity onPress={() => router.replace("/(tabs)")}>
-        <Text>Go to Home Screen</Text>
-      </TouchableOpacity>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+        <StatusBar style='light'/>
+            <ImageBackground source={require("../assets/3d-earth-planet-shape.jpg")} style={styles.imageBackground}
+                             resizeMode="cover">
+                <View style={styles.main}>
+                    {/* Animated Title */}
+                    <Animated.Text style={styles.title} entering={FadeInRight.delay(300).duration(500)}>Stay
+                        Updated!</Animated.Text>
+
+                    {/* Animated Subtitle */}
+                    <Animated.Text style={styles.subtitle} entering={FadeInRight.delay(700).duration(500)}>Welcome to
+                        our NovaNews App
+                    </Animated.Text>
+
+                    {/* Button Section */}
+                    <Animated.View entering={FadeInDown.delay(1200).duration(500)}>
+                        {/* Navigate to Home Screen */}
+                        <Animated.View entering={FlipInEasyX.delay(600).duration(500)}
+                                       exiting={FlipOutEasyX.delay(600).duration(500)}>
+                            <TouchableOpacity style={styles.button} onPress={() => router.replace("/(tabs)")}>
+                                <Text style={styles.buttonText}>Get Started</Text>
+                            </TouchableOpacity>
+                        </Animated.View>
+
+                        {/* Navigate to Login Screen */}
+                        <Animated.View entering={FlipInEasyX.delay(800).duration(500)}
+                                       exiting={FlipOutEasyX.delay(800).duration(500)}>
+                            <TouchableOpacity style={styles.button} onPress={() => router.push("/login")}>
+                                <Text style={styles.buttonText}>Login</Text>
+                            </TouchableOpacity>
+                        </Animated.View>
+                    </Animated.View>
+                </View>
+            </ImageBackground>
+        </View>
+    );
 };
 
 export default Page;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+    container: {
+        flex: 1,
+    },
+    imageBackground: {
+        flex: 1,
+        width: "100%",
+        height: "100%",
+    },
+    main: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        paddingBottom: 50,
+        gap: 10,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        width: "100%",
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: "bold",
+        color: "#fff",
+        textAlign: "center",
+    },
+    subtitle: {
+        fontSize: 20,
+        color: "#fff",
+        textAlign: "center",
+        marginBottom: 20,
+    },
+    button: {
+        marginTop: 10,
+        backgroundColor: "#4DB6AC",
+        paddingVertical: 15,
+        marginVertical: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 24,
+        borderRadius: 25,
+        width:'100%',
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 18,
+        fontWeight: "bold",
+    },
 });
