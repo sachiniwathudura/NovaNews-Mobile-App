bbdb72840c7465f4ae3dda1beeb745f4fa9479d6
sachiniwathudura <sachiniwathudura@gmail.com>
2025-02-27 02:38:45 +0530

    add - colour and countryList

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,11 +1,26 @@
 export const Colors = {
   background: '#F4F4F4',
-  tint: '#FF4C4C',
+  tint: '#00897B',
   tabIconDefault: '#687076',
-  tabIconSelected: '#FF4C4C',
+  tabIconSelected: '#4DB6AC',
   white: '#fff',
   black: '#333',
   darkGrey: '#666',
   lightGrey: '#999',
   softText: '#555',
+
+  light: {
+    background: '#ffffff',
+    white: '#ffffff',
+    black: '#000000',
+    lightGrey: '#cccccc',
+    tint: '#3e3e3e',
+  },
+  dark: {
+    background: '#121212',
+    white: '#1e1e1e',
+    black: '#ffffff',
+    lightGrey: '#444444',
+    tint: '#767577',
+  },
 };
