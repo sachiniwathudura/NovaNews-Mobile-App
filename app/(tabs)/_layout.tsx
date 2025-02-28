da7ee8e95e469dfe3014bb7784a1515e3a2d5b4b
sachiniwathudura <sachiniwathudura@gmail.com>
2025-02-28 16:17:49 +0530

    add login signup to layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,34 +4,46 @@ import {TabBar} from "../../components/TabBar";
 import headerShownContext from "@react-navigation/elements/src/Header/HeaderShownContext";
 
 const TabLayout = () => {
-  return (
-    <Tabs tabBar={(props) => <TabBar {...props} />} screenOptions={{headerShown:false}}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-        }}
-      />
-      <Tabs.Screen
-        name="discover"
-        options={{
-          title: "Discover",
-        }}
-      />
-      <Tabs.Screen
-        name="saved"
-        options={{
-          title: "Saved",
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "Settings",
-        }}
-      />
-    </Tabs>
-  )
+    return (
+        <Tabs tabBar={(props) => <TabBar {...props} />} screenOptions={{headerShown:false}}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                }}
+            />
+            <Tabs.Screen
+                name="login"
+                options={{
+                    title: "login",
+                }}
+            />
+            <Tabs.Screen
+                name="signup"
+                options={{
+                    title: "signup",
+                }}
+            />
+            <Tabs.Screen
+                name="discover"
+                options={{
+                    title: "Discover",
+                }}
+            />
+            <Tabs.Screen
+                name="saved"
+                options={{
+                    title: "Saved",
+                }}
+            />
+            <Tabs.Screen
+                name="settings"
+                options={{
+                    title: "Settings",
+                }}
+            />
+        </Tabs>
+    )
 }
 
 export default TabLayout
